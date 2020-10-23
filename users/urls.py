302769f87ffc85363b8fb8b9d5607734d8472282
TomJames <tom926344@gmail.com>
2020-10-23 18:47:02 +0800

    项目learning_log

---
+++ users/urls.py
@@ -0,0 +1,19 @@
+''' 定义users的所有URL模式 '''
+
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView
+
+app_name='users'
+urlpatterns = [
+	#登录页面
+	path('login/', LoginView.as_view(template_name='users/login.html'),name='login'),
+    
+    #注销
+    path('logout/', views.logout_view, name='logout'),
+    
+    #注册页面
+    path('register/', views.register, name='register'),
+    
+]
+
